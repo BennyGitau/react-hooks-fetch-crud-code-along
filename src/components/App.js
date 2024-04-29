d326e17a1d7360642a3eaeaf857beaa077c60605
BensonGitau <bensonkamaugitau@gmail.com>
2024-04-30 01:02:46 +0300

    Complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,16 @@ import Header from "./Header";
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false);
-
+  
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList />
+        <ShoppingList />
     </div>
   );
 }
